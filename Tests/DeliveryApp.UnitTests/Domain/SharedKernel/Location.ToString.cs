2955a4dd32006af378650586070800da429cb0c1
Mikhail Ushanov <mikhail.ushanov@gmail.com>
2025-07-27 16:39:21 +0300

    Location.ToString

--- Tests/DeliveryApp.UnitTests/Domain/SharedKernel/Location.ToString.cs
+++ Tests/DeliveryApp.UnitTests/Domain/SharedKernel/Location.ToString.cs
@@ -7,9 +7,9 @@ namespace DeliveryApp.UnitTests.Domain.SharedKernel;
 public partial class LocationShould
 {
     [Theory]
-    [InlineData(1, 1, "(1, 1)")]
-    [InlineData(5, 7, "(5, 7)")]
-    [InlineData(10, 10, "(10, 10)")]
+    [InlineData(1, 1, "(X:1, Y:1)")]
+    [InlineData(5, 7, "(X:5, Y:7)")]
+    [InlineData(10, 10, "(X:10, Y:10)")]
     public void FormatAsCoordinateString_WhenConverted(int x, int y, string expected)
     {
         // Arrange
