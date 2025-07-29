5b818cfc792ac128efba1874dda3425a4ef10a52
Mikhail Ushanov <mikhail.ushanov@gmail.com>
2025-07-30 01:59:46 +0300

    Скорость - ValueObject

---
+++ DeliveryApp.Core/Domain/SharedKernel/Speed.cs
@@ -0,0 +1,54 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using CSharpFunctionalExtensions;
+
+namespace DeliveryApp.Core.Domain.SharedKernel;
+
+/// <summary>
+/// Value Object для представления скорости
+/// </summary>
+public class Speed : ValueObject
+{
+    /// <summary>
+    /// Значение скорости
+    /// </summary>
+    public int Value { get; }
+    
+    /// <summary>
+    /// Конструктор для создания значения скорости
+    /// </summary>
+    /// <param name="value">Значение скорости</param>
+    private Speed(int value)
+    {
+        Value = value;
+    }
+    
+    /// <summary>
+    /// Создает новое значение скорости
+    /// </summary>
+    /// <param name="value"></param>
+    /// <returns></returns>
+    /// <exception cref="ArgumentException"></exception>
+    /// <returns>Speed</returns> 
+    public static Speed Create(int value)
+    {
+        if (value <= 0)
+            throw new ArgumentException("Скорость должен быть больше 0", nameof(value));
+        
+        return new Speed(value);
+    }
+    
+    [ExcludeFromCodeCoverage]
+    protected override IEnumerable<object> GetEqualityComponents()
+    {
+        yield return Value;
+    }
+    
+    /// <summary>
+    /// Строковое представление скорости
+    /// </summary>
+    [ExcludeFromCodeCoverage]
+    public override string ToString()
+    {
+        return $"{Value}";
+    }
+}
---
+++ Tests/DeliveryApp.UnitTests/Domain/SharedKernel/Speed.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using DeliveryApp.Core.Domain.SharedKernel;
+using Xunit;
+
+namespace DeliveryApp.UnitTests.Domain.SharedKernel;
+
+public class SpeedShould
+    {
+        [Theory]
+        [InlineData(1)]
+        [InlineData(100)]
+        [InlineData(999999)]
+        public void BeCreated_WhenGivenValidValue(int validValue)
+        {
+            // Act
+            var speed = Speed.Create(validValue);
+
+            // Assert
+            Assert.Equal(validValue, speed.Value);
+        }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(-1)]
+        [InlineData(-100)]
+        public void ThrowArgumentException_WhenGivenInvalidValue(int invalidValue)
+        {
+            // Act & Assert
+            var exception = Assert.Throws<ArgumentException>(() => Speed.Create(invalidValue));
+            Assert.Equal("value", exception.ParamName);
+        }
+    }
+    
